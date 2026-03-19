325b2a64625464dd997edf53e5362cb505b74ef3
HanleyKingston <hkingsto@gmail.com>
2026-03-19 08:57:21 -0700

    add comment to script

--- workshop_scripts/decode_chunk.py
+++ workshop_scripts/decode_chunk.py
@@ -1,6 +1,8 @@
 
+# Load data
 str9_encrypted = open("..\\encrypted_data\\9.txt").read()
 
+# Decryption
 def decrypt(encrypted_message:str, key:int) -> str:
     message = ""
     for i in range(len(encrypted_message)):
@@ -9,9 +11,9 @@ def decrypt(encrypted_message:str, key:int) -> str:
         message += decrypted_char
     return message
 
-
 str9_decrypted = decrypt(encrypted_message = str9_encrypted, key = 9)
 
+# Save Decrypted data
 with open("..\\workshop_data\\9.txt", "w") as file:
     file.write(str9_decrypted)
 
